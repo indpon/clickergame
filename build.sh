8c4b7681cec54b301e3b4a2d4fedc684a2493339
indpon <indpon@yahoo.com>
2026-02-23 02:13:01 -0500

    things

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 
 
-g++ src/main.cpp -o build/game -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
-
+g++ src/main.cpp -o build/game -fpermissive -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
 ./build/game
